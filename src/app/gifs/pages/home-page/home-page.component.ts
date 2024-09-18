2911bc383dcb0f308f9d461d31773079ed8a696c
sebastian-VB <sebas99peru@gmail.com>
2024-09-18 12:33:14 -0500

    Change selector

--- src/app/gifs/pages/home-page/home-page.component.ts
+++ src/app/gifs/pages/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home-page',
+  selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
