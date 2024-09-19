61ab7659cdd246d8170ce358557e07a551816c67
sebastian-VB <sebas99peru@gmail.com>
2024-09-19 13:39:44 -0500

    Add data type in response

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,13 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+
+  public gifList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
   private apiKey: string = 'k8ViIiUKQNH0jqCZG4wVWtqik2KBjdKh';
@@ -26,12 +29,12 @@ export class GifsService {
       .set('limit', '10')
       .set('q', tag)
 
-    this.http.get(`${this.serviceUrl}/search`, {params})
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
       .subscribe(resp =>{
-        console.log(resp)
+        this.gifList = resp.data;
+        console.log({gifs: this.gifList})
       });
 
-    // console.log(this._tagsHistory);
   }
 
   private organizeHistory(tag: string): void{
