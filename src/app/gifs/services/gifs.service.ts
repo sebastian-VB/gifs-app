2c5e4064b9019669b4bb75d1a93d139745374029
sebastian-VB <sebas99peru@gmail.com>
2024-09-19 13:14:46 -0500

    Add code for get list of gifs of the api

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,8 +7,10 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'k8ViIiUKQNH0jqCZG4wVWtqik2KBjdKh';
+  private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory(): string[]{
     return [...this._tagsHistory]; //crear una copia del arreglo
@@ -16,10 +19,19 @@ export class GifsService {
   searchTag(tag: string): void{
 
     if(tag.length === 0) return;
-
     this.organizeHistory(tag);
 
-    console.log(this._tagsHistory);
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get(`${this.serviceUrl}/search`, {params})
+      .subscribe(resp =>{
+        console.log(resp)
+      });
+
+    // console.log(this._tagsHistory);
   }
 
   private organizeHistory(tag: string): void{
