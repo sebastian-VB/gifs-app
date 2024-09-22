e5366c7004764b69f6ca04768500ae3e86b1573c
sebastian-VB <sebas99peru@gmail.com>
2024-09-22 00:05:11 -0500

    Change component for show loader

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
   styleUrl: './lazy-image.component.css'
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit{
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('URL property is required');
+  }
+
+  onLoad(): void{
+    this.hasLoaded = true;
+  }
 
 }
